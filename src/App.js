b00031fb93d050df1c2b2e34b84a8a740dacf2c1
Jamal Ruiz <jamalruiz@jamals-air-2.mynetworksettings.com>
2022-05-15 13:27:13 -0400

    Layout Complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import {useState} from "react"
+import Birds from "./Components/Card"
+import Cart from "./Components/Cart"
+import Checkout from "./Components/Checkout"
 const App = () => {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <main>
+       <h1>Bird Sanctuary</h1>
+       <Cart></Cart>
+       <Checkout></Checkout>
+      <Birds></Birds>
+      </main>
     </div>
   );
 };
---
+++ src/Components/Cart.js
@@ -0,0 +1,11 @@
+import birdData from "../data/birds";
+const Cart = () => {
+return (
+    <div>
+        <h1>Cart</h1>
+        <h4>Discount:</h4>
+        <h4>Totals:</h4>
+    </div>
+)
+}
+export default Cart
