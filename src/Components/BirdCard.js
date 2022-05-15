b623eac2d91ffa7846a1b5acc6f3e77e07f4a5a2
Samantha Figueroa <samanthafigueroa@pursuit.org>
2022-05-15 17:45:02 -0400

    almost done

---
+++ src/Components/BirdCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BirdCards from './BirdCards';
+
+const BirdCard = ({name, amount, img, id, cart, setCart}) => {
+    return (
+        <div className="card">
+            <h4>{name}</h4>
+            <p>${amount.toFixed()}</p>
+            <img src={img} alt={name} width='200px'/>
+            <br/>
+            <button className="birds" value={id} onClick={()=>setCart([...cart, {name, amount}])}>Adopt</button>
+        </div>
+    )
+}
+
+export default BirdCard;
